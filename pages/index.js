1316b14c88f8381f7c9b08e332efa81d3ef4e9ea
joacodiler <jdilernia99@gmail.com>
2023-04-28 17:00:41 -0300

    iniciar sesion con google

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home() {
+  const { data: session } = useSession();
+  if(!session){
   return (
     <div className="bg-blue-900 w-screen h-screen flex items-center">
       <div className="text-center w-full"> 
-      <button className="bg-white p-2 px-4 rounded-lg"> Login With Google </button>
+      <button onClick={()=> signIn('google')} className="bg-white p-2 px-4 rounded-lg"> Login With Google </button>
       </div>
     </div>
-  )
+  );
+  }
+  return (
+    <div>logged in {session.user.email}</div>
+);
 }
