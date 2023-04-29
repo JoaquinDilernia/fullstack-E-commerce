c41d6b8d052b1403a96d386eec7fc5e6c4d53b43
joacodiler <jdilernia99@gmail.com>
2023-04-29 02:43:06 -0300

    page category y products basta por hoy

--- components/Nav.js
+++ components/Nav.js
@@ -2,12 +2,12 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Nav() {
-    const inactiveLink ='flex gap-1 p-1';
-    const activeLink =inactiveLink +' bg-white text-blue-900 rounded-l-lg';
-    const router = useRouter(); 
-    const {pathname} = router;
+    const inactiveLink = 'flex gap-1 p-1';
+    const activeLink = inactiveLink + ' bg-white text-blue-900 rounded-l-lg';
+    const router = useRouter();
+    const { pathname } = router;
     return (
-        
+
         <aside className="text-white p-4 pr-0">
             <Link href={'/'} className="flex gap-1 mb-4 mr-3">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -16,7 +16,7 @@ export default function Nav() {
                 <span className=""> EcommerceAdmin</span>
             </Link>
             <nav className="flex flex-col gap-2">
-                <Link href={'/'}className={pathname=== '/' ? activeLink : inactiveLink}>
+                <Link href={'/'} className={pathname === '/' ? activeLink : inactiveLink}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                     </svg>
@@ -28,6 +28,13 @@ export default function Nav() {
                     </svg>
 
                     Products</Link>
+
+                <Link href={'/categories'} className={pathname.includes('/categories') ? activeLink : inactiveLink}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"className="w-6 h-6" >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    </svg>
+                    Categories
+                </Link>
 
                 <Link href={'/orders'} className={pathname.includes('/orders') ? activeLink : inactiveLink}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
