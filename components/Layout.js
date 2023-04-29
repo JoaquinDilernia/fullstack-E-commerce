113bd5e4a4b54a4e8c220bb0e29c3ad524e8b76c
joacodiler <jdilernia99@gmail.com>
2023-04-29 00:41:25 -0300

    conexion a mongo y layout con next auth google

---
+++ components/Layout.js
@@ -0,0 +1,26 @@
+import Nav from "@/components/Nav";
+import { useSession, signIn, signOut } from "next-auth/react"
+
+
+export default function Layout({children}) {
+
+    const { data: session } = useSession();
+
+    if (!session) {
+
+        return (
+            <div className="bg-blue-900 w-screen h-screen flex items-center">
+                <div className="text-center w-full">
+                    <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg"> Login With Google </button>
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div className="bg-blue-900 min-h-screen flex">
+            <Nav />
+            <div className="bg-white flex-grow mt-1 mr-2 mb-2 rounded-lg p-4">{children}</div>
+            
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
+
+
 export default function Home() {
-  const { data: session } = useSession();
-  if(!session){
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full"> 
-      <button onClick={()=> signIn('google')} className="bg-white p-2 px-4 rounded-lg"> Login With Google </button>
-      </div>
-    </div>
-  );
-  }
-  return (
-    <div>logged in {session.user.email}</div>
-);
+  return <Layout>
+    test
+  </Layout>
+
 }
